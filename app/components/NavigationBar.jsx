ebbf3cc216d01a886b698181f661c9056a8debbe
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-04-28 19:28:52 +0800

    adding burger button and responsive

--- app/components/NavigationBar.jsx
+++ app/components/NavigationBar.jsx
@@ -2,90 +2,91 @@
 import Navlist from "./Navlist";
 import Image from "next/image";
 import Link from "next/link";
+import navItem from "../Data/menu.json";
 import { useState } from "react";
 
+import { createMedia } from "@artsy/fresnel";
+
 const NavigationBar = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { MediaContextProvider, Media } = createMedia({
+    // breakpoints values can be either strings or integers
+    breakpoints: {
+      sm: 0,
+      md: 768,
+      lg: 1024,
+      xl: 1192,
+    },
+  });
 
   const handleToggle = () => {
     setIsOpen((toggle) => !toggle);
   };
-
-  const navItem = [
-    {
-      title: "Home",
-      link: "/",
-    },
-    {
-      title: "About",
-      link: "/about",
-    },
-    {
-      title: "Project",
-      link: "/project",
-    },
-    {
-      title: "Contact",
-      link: "/contact",
-    },
-  ];
+  const handleClickItem = () => {
+    setIsOpen(false);
+  };
 
   return (
     // Home Nav
-    <div id="nav" className="backdrop-blur-sm z-50 relative">
-      <div className="wrapper">
-        {/* Mobile */}
-        <div>
-          <nav className="flex justify-between items-center min-h-14 px-4">
-            <Link href={"/"}>
-              <Image
-                src="/pp.png"
-                width={51}
-                height={51}
-                alt="Rhysin Villahermosa Logo"
-              />
-            </Link>
-            <div
-              className="flex flex-col w-9 h-9 justify-evenly z-50"
-              onClick={handleToggle}
-            >
-              <span className="w-full h-1 bg-white rounded-md"></span>
-              <span className="w-full h-1 bg-white rounded-md"></span>
-              <span className="w-full h-1 bg-white rounded-md"></span>
-            </div>
-            {isOpen && (
-              <section className="fixed top-0 left-0 w-screen h-screen bg-priColor-700 flex items-center justify-center">
-                <div className="flex flex-col gap-y-5 text-8xl">
-                  {navItem.map((item) => (
-                    <Navlist item={item} key={item.title}></Navlist>
-                  ))}
-                </div>
-              </section>
-            )}
-          </nav>
-        </div>
+    <MediaContextProvider>
+      {/* Mobile */}
+      <Media lessThan="md">
+        <nav className="flex justify-between items-center min-h-14 px-4">
+          <Link href={"/"}>
+            <Image
+              src="/pp.png"
+              width={51}
+              height={51}
+              alt="Rhysin Villahermosa Logo"
+            />
+          </Link>
+          <div
+            className="flex flex-col w-9 h-9 justify-evenly z-50"
+            onClick={handleToggle}
+          >
+            <span className="w-full h-1 bg-white rounded-md"></span>
+            <span className="w-full h-1 bg-white rounded-md"></span>
+            <span className="w-full h-1 bg-white rounded-md"></span>
+          </div>
 
-        {/* Desktop */}
-        <div className="md:block hidden">
-          <nav className="flex justify-between items-center min-h-14">
-            <Link href={"/"}>
-              <Image
-                src="/pp.png"
-                width={51}
-                height={51}
-                alt="Rhysin Villahermosa Logo"
-              />
-            </Link>
-
-            <section className="flex gap-x-5 font-secFont text-white">
-              {navItem.map((item) => (
-                <Navlist item={item} key={item.title}></Navlist>
-              ))}
+          {isOpen && (
+            <section className="fixed inset-0 bg-triColor-900 flex items-center justify-center z-40">
+              <div className="flex flex-col gap-y-5 text-8xl">
+                {navItem.map((item) => (
+                  <div onClick={handleClickItem} key={item.title}>
+                    <Navlist item={item} />
+                  </div>
+                ))}
+              </div>
             </section>
-          </nav>
+          )}
+        </nav>
+      </Media>
+      {/* Desktop */}
+      <Media greaterThanOrEqual="md">
+        <div id="nav" className="backdrop-blur-sm relative z-50">
+          <div className="wrapper">
+            <nav className="flex justify-between items-center min-h-14">
+              <Link href={"/"}>
+                <Image
+                  src="/pp.png"
+                  width={51}
+                  height={51}
+                  alt="Rhysin Villahermosa Logo"
+                />
+              </Link>
+
+              <section className="flex gap-x-5 font-secFont text-white">
+                {navItem.map((item) => (
+                  <Navlist item={item} key={item.title}></Navlist>
+                ))}
+              </section>
+            </nav>
+          </div>
         </div>
-      </div>
-    </div>
+      </Media>
+    </MediaContextProvider>
   );
 };
 
