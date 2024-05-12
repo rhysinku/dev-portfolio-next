72e64742c58a5590f175fafc0edd124fa5d76987
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-05-12 17:20:05 +0800

    update pdf file

--- app/components/ProjectComponents/ProjectBox.jsx
+++ app/components/ProjectComponents/ProjectBox.jsx
@@ -15,7 +15,7 @@ const ProjectBox = () => {
   return (
     <div ref={carouselRef} className="relative h-[365vh] flex flex-wrap">
       <div className="sticky top-0 h-screen flex items-center overflow-hidden pt-7">
-        <motion.div className="flex " style={{ x }}>
+        <motion.div className="flex  snap-x" style={{ x }}>
           {projectInfo.map((project, index) => (
             <ProjectCard key={index} project={project} />
           ))}
