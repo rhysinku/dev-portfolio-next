4dea4bf9a2778034501905f4fc8c3be2a2a8e687
RhysinV <rhyvillahermosa@proweaver.net>
2024-06-11 20:55:17 +0800

    addTool Init feat: add images for Tools

---
+++ app/gallery/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function GalleryPage() {
+
+  return (
+    <div className='n-[calc(100vh-3.5rem)]'>
+        <div className="wrapper">
+            <div className="h-screen">
+
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default GalleryPage
